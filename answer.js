23674583b8c0f76110a9debd4d2dd8f78cd1b226
tsuyopon-xyz <tsuyoshi4business+tsuyopon.xyz@gmail.com>
2018-11-16 17:30:00 +0900

    Created the filter function that behaves like the Array.prototype.filter().

--- answer.js
+++ answer.js
@@ -1,136 +1,146 @@
-// タスク一覧の初期データとして次の値を配列で持つ
-//   - 変数名は「todos」とする
-//   - 初期データとして次の3つの値を配列に格納していおく
-//     1. 掃除
-//     2. 買い物
-//     3. 散歩
-const todos = ['掃除', '買い物', '散歩'];
-
-// Todoリストの機能を入力された文字によって使い分けるための文字列を配列で所持する
-//   - 変数名は「commands」とする
-//   - commandsに格納する文字列は次の順番で保持する
-//     1. 確認
-//     2. 追加
-//     3. 削除
-//     4. 終了
-const commands = ['確認', '追加', '削除', '終了'];
-
-// promptで入力したものを保持しておくための変数「input」を用意する
-let input;
-
-// 「終了」と入力されたらループを終了するwhileループを作成する
-//   - whileループの中でprompt関数を使って、値を入力できるようにして、入力された内容は変数「input」に格納する
-//     - prompt: https://developer.mozilla.org/ja/docs/Web/API/Window/prompt
-//   - 変数inputの値によって、次の機能を実行する
-//     1. 「確認」と入力された時: showTodos関数を実行する
-//     2. 「追加」と入力された時: createTodo関数を実行する
-//     3. 「削除」と入力された時: deleteTodo関数を実行する
-//     4. 「終了」と入力された時: Consoleに'終了します'と表示して、Whileループを終了する
-//   - 「確認」「追加」「削除」「終了」以外がinputに代入されたら「「確認,追加,削除,終了」以外は入力できません」とConsoleに表示する
-while(input !== commands[3]) {
-  input = prompt('「'+ commands + '」の4つのいずれかを入力してください');
-  if (input === commands[0]) {
-    showTodos();
-  } else if (input === commands[1]) {
-    createTodo();
-  } else if (input === commands[2]) {
-    deleteTodo();
-  } else if (input === commands[3]) {
-    console.log('終了します');
-  } else {
-    console.log('「'+ commands + '」以外は入力できません');
-  }
-}
-
-// Whileループを抜けた後もタスク一覧を表示する(最終的なタスク一覧を確認するため)
-showTodos();
+// 配列の組み込みメソッドと同等の機能を持った関数を作る
 
 /**
- * 「確認」が入力されたときに実行される関数
- *    - 以下の形式でtodosの内容を表示する (todosの中に「'掃除', '買い物', '散歩'」が格納されている場合)
- *
- *   表示される内容(ヒント、forループもしくはforEachをつかう)
- *   ↓
- *   ========================
- *   現在持っているのタスク一覧
- *   ========================
- *   0 : 掃除
- *   1 : 買い物
- *   2 : 散歩
- *
- *
- *    - todosの中身がからの時は以下のように表示する
- *
- *   表示される内容
- *   ↓
- *   ========================
- *   現在持っているのタスク一覧
- *   ========================
- *   タスクなし
+ * 前提: 配列の組み込みメソッドは使わない
  */
-function showTodos() {
-  console.log('========================');
-  console.log('現在持っているのタスク一覧');
-  console.log('========================');
-
-  if (todos.length > 0) {
-    todos.forEach((todo, index) => {
-      console.log(index + ' : ' + todo);
-    });
-  } else {
-    console.log('タスク無し');
-  }
-}
 
 /**
- * 「追加」が入力されたときに実行される関数
- *    - promptを使って追加するタスクを入力できるようにする
- *      - 入力ボックスに表示するメッセージは「'タスクを入力してください'」とする
- *    - promptに値が入力されないで「OK」ボタンがおされたらalertで「'何も入力されていないためスキップします'」と表示する
- *    - promptに値が入力された状態で「OK」ボタンがおされたら次の処理をする
- *      1. 入力された内容をtodos(配列)の末尾に追加する。
- *      2. 「'新しいタスクを追加しました。'」とalert表示する
- *      3. showTodos関数を実行して、現在保持しているタスク一覧を表示する
+ * 課題1: 「Array.prototype.forEach()」と同等の機能を持つ関数を作る
+ *   - 「Array.prototype.forEach()」のドキュメント
+ *     - https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+ *   - 関数名は「each」とする
+ *   - each関数は次の2つの引数を持つ
+ *     - 第1引数: 配列(引数名はarrayとする)
+ *     - 第2引数: コールバック関数(引数名はcallbackとする)
+ *       - コールバック関数の内容は、each関数の第1引数で受け取った配列をイテレーションする機能を持つ
+ *       - コールバックは次の2つの引数を持つ
+ *         - 第1引数: each関数の第1引数で受け取った配列の各要素を1ループごとにセットする
+ *         - 第2引数: コールバック関数の第1引数で受け取った要素に対応するインデックス番号をセットする
+ *       - コールバック関数は戻り値を持たない
+ *   - each関数は戻り値を持たない
+ *   - 実装したeach関数が意図通り動くか確かめるためのコードを記述する
+ *     - コールバック関数の中でconsole.logを使って、コールバック関数の第1引数と第2引数の値を確認する
+ *     - each関数内でarrayをループさせるにはfor文を使う
+ *
+ *
+ * each関数の実装の答えは以下の学習コンテンツのスライド内に記述されているので、実装イメージがつかなかったら参考にしてもOK
+ *
+ * JavaScriptの配列でイテレーション処理をする
+ * https://tsuyopon.xyz/learning-contents/web-dev/javascript/javascript-grammar/how-to-iterate-an-array-in-js/
  */
-function createTodo() {
-  const todo = prompt('タスクを入力してください');
-  if (todo) {
-    todos.push(todo);
-    alert('新しいタスクを追加しました。');
-    showTodos();
-  } else {
-    alert('何も入力されていないためスキップします');
+
+// ここでeach関数を作る
+const each = (array, callback) => {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i], i);
   }
-}
+};
+
+// ここでeach関数の挙動を確かめる
+const inputArrayForEach = [1, 2, 3, 4];
+const returnedValueOfEach = each(inputArrayForEach, (value, index) => {
+  console.log('each関数のコールバック関数内 index: ' + index, '値: ' + value, );
+});
+
+console.log('each関数の入力値: ', inputArrayForEach);
+console.log('each関数の戻り値: ', returnedValueOfEach);
 
 /**
- * 「削除」が入力されたときに実行される関数
- *    - promptを使って削除するタスクのインデックス番号を入力できるようにする
- *      - 入力ボックスに表示するメッセージは「'削除するタスクの番号を指定してください'」とする
- *    - promptで入力された数字は `文字列` として扱われるため、数値に変換する
- *      - 数字('1')を数値(1)に変換するにはparseInt関数をつかう
- *        - parseInt: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/parseInt
- *    - promptに値が入力された値が不正な場合は「'不正な値のためスキップします'」とalert表示する
- *      - 不正な値とは、配列のインデックス番号以外の値を指す。(配列に5個値があれば、「0, 1, 2, 3, 4」だけが正しい値で、それ以外は不正な値になる)
- *      - parseIntした結果「NaN」という値が出たときの対応策として「isNaN」関数が使える
- *        - isNaN: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/isNaN
- *    - promptに値が入力された値が正しければ次の処理をする
- *      1. 配列の組み込みメソッド「splice」を使って、入力されたインデックス番号に対応した値を配列から削除する
- *        - splice: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
- *        - 削除した値は変数deletedTodosという変数に代入する(上記ドキュメントを読むと分かる通り、削除した値が1つだけでも戻り値は配列となることに注意)
- *      2. 「'〇〇を削除しました'」とalert表示する
- *        - 「〇〇」には削除した「タスク名」が入る
- *          - 例: 「掃除」を削除した場合は、「'掃除を削除しました'」とalert表示する
- *      3. showTodos関数を実行して、現在保持しているタスク一覧を表示する
+ * 課題2: 「Array.prototype.map()」と同等の機能を持つ関数を作る
+ *   - 「Array.prototype.map()」のドキュメント
+ *     - https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+ *   - 関数名は「map」とする
+ *   - map関数は次の2つの引数を持つ
+ *     - 第1引数: 配列(引数名はarrayとする)
+ *     - 第2引数: コールバック関数(引数名はcallbackとする)
+ *       - コールバック関数の内容は、map関数の第1引数で受け取った配列をイテレーションする機能を持つ
+ *       - コールバックは次の2つの引数を持つ
+ *         - 第1引数: map関数の第1引数で受け取った配列の各要素を1ループごとにセットする
+ *         - 第2引数: コールバック関数の第1引数で受け取った要素に対応するインデックス番号をセットする
+ *       - コールバック関数は戻り値を持つ
+ *         - コールバック関数内でreturnした値がmap関数の配列の要素としてセットされる
+ *   - map関数は戻り値を持つ
+ *     - map関数の戻り値はコールバック関数のそれぞれの戻り値を格納した配列となる
+ *       - 例: 以下のnewArrayには[2, 4, 6]という配列が格納される。
+ *         - const newArray = map([1, 2, 3], (value, index) => {
+ *             return value * 2; // valueは「1, 2, 3」 と順番にセットされて、「value * 2」で2倍にしているため[1, 2, 3]は[2, 4, 6]となる
+ *           });
+ *   - 実装したmap関数が意図通り動くか確かめるためのコードを記述する
+ *     - map関数の返り値をconsole.logで出力する
+ *     - map関数内のイテレーション処理には課題1で作ったeach関数を利用する
+ *
+ * 上の文章で実装する内容を理解するよりも、「Array.prototype.map()」のドキュメントの例を参考にmapがどんな挙動をするのか理解するのか理解したほうが早いかも。
  */
-function deleteTodo() {
-  const numberString = prompt('削除するタスクの番号を指定してください');
-  const parsedNumber = parseInt(numberString, 10);
-  if (isNaN(parsedNumber) || parsedNumber >= todos.length || parsedNumber < 0) {
-    alert('不正な値のためスキップします');
-  } else {
-    const deletedTodos = todos.splice(parsedNumber, 1);
-    alert( deletedTodos[0] + 'を削除しました。');
-    showTodos();
-  }
-}
+
+// ここでmap関数を実装する
+const map = (array, callback) => {
+  const newArray = [];
+  each(array, (value, index) => {
+    const newValue = callback(value, index);
+    newArray.push(newValue);
+  });
+
+  return newArray;
+};
+
+// ここでmap関数の挙動を確認する
+const inputArrayForMap = [1, 2, 3];
+const returnedValueOfMap = map(inputArrayForMap, (value, index) => {
+  console.log('map関数のコールバック関数内 index: ' + index, '値: ' + value, );
+  return value * 2;
+});
+
+console.log('map関数の入力値: ', inputArrayForMap);
+console.log('map関数の戻り値: ', returnedValueOfMap);
+
+
+/**
+ * 課題3: 「Array.prototype.filter()」と同等の機能を持つ関数を作る
+ *   - 「Array.prototype.filter()」のドキュメント
+ *     - https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+ *   - 関数名は「filter」とする
+ *   - filter関数は次の2つの引数を持つ
+ *     - 第1引数: 配列(引数名はarrayとする)
+ *     - 第2引数: コールバック関数(引数名はcallbackとする)
+ *       - コールバック関数の内容は、filter関数の第1引数で受け取った配列をイテレーションする機能を持つ
+ *       - コールバックは次の2つの引数を持つ
+ *         - 第1引数: filter関数の第1引数で受け取った配列の各要素を1ループごとにセットする
+ *         - 第2引数: コールバック関数の第1引数で受け取った要素に対応するインデックス番号をセットする
+ *       - コールバック関数は戻り値を持つ
+ *         - コールバック関数内で真偽値(true or false)をreturnする
+ *           - trueを返した場合は、現在セットされている要素をfilter関数の戻り値である配列に格納する
+ *           - falseを返した場合は、現在セットされている要素をfilter関数の戻り値である配列に含めないようにする
+ *   - filter関数は戻り値を持つ
+ *     - filter関数の戻り値はコールバック関数の戻り値がtrueだった要素を格納した配列となる
+ *       - 例: 以下のfilteredArrayには[2, 4]という配列が格納される。
+ *         - const filteredArray = filter([1, 2, 3, 4, 5], (value, index) => {
+ *             return value % 2 === 0; // valueは「1, 2, 3, 4, 5」 と順番にセットされて、「value % 2 === 0」で偶数がtrueになるので「2, 4」trueとなり[2, 4]という配列が返る
+ *           });
+ *   - 実装したfilter関数が意図通り動くか確かめるためのコードを記述する
+ *     - filter関数の返り値をconsole.logで出力する
+ *     - filter関数内のイテレーション処理には課題1で作ったeach関数を利用する
+ *
+ * 上の文章で実装する内容を理解するよりも、「Array.prototype.filter()」のドキュメントの例を参考にfilterがどんな挙動をするのか理解するのか理解したほうが早いかも。
+ */
+
+// ここでfilter関数を作る
+const filter = (array, callback) => {
+  const filteredArray = [];
+  each(array, (value, index) => {
+    if ( callback(value, index) ) {
+      filteredArray.push(value);
+    }
+  });
+
+  return filteredArray;
+};
+
+// ここでfilter関数の挙動を確認する
+const inputArrayForFilter = [1, 2, 3, 4, 5];
+const returnedValueOfFilter = filter(inputArrayForFilter, (value, index) => {
+  console.log('filter関数のコールバック関数内 index: ' + index, '値: ' + value, );
+  return value % 2 === 0;
+});
+
+console.log('filter関数の入力値: ', inputArrayForFilter);
+console.log('filter関数の戻り値: ', returnedValueOfFilter);
--- main.js
+++ main.js
@@ -30,17 +30,10 @@
  */
 
 // ここでeach関数を作る
-const each = (array, callback) => {
-  for (let i = 0; i < array.length; i++) {
-    callback(array[i], i);
-  }
-};
+
 
 // ここでeach関数の挙動を確かめる
-const returnedValueOfEach = each(['one', 'two', 'three'], (value, index) => {
-  console.log('each関数のコールバック関数内 index: ' + index, '値: ' + value, );
-});
-console.log('each関数の戻り値: ', returnedValueOfEach);
+
 
 /**
  * 課題2: 「Array.prototype.map()」と同等の機能を持つ関数を作る
@@ -58,9 +51,9 @@ console.log('each関数の戻り値: ', returnedValueOfEach);
  *         - コールバック関数内でreturnした値がmap関数の配列の要素としてセットされる
  *   - map関数は戻り値を持つ
  *     - map関数の戻り値はコールバック関数のそれぞれの戻り値を格納した配列となる
- *       - 例: 以下のnewArrayには[2, 4 ,6]という配列が格納される。
+ *       - 例: 以下のnewArrayには[2, 4, 6]という配列が格納される。
  *         - const newArray = map([1, 2, 3], (value, index) => {
- *             return value * 2; // valueは「1, 2, 3 」 と順番にセットされて、それ 「value * 2」で2倍にしているため[1, 2, 3]は[2, 4, 6]となる
+ *             return value * 2; // valueは「1, 2, 3」 と順番にセットされて、「value * 2」で2倍にしているため[1, 2, 3]は[2, 4, 6]となる
  *           });
  *   - 実装したmap関数が意図通り動くか確かめるためのコードを記述する
  *     - map関数の返り値をconsole.logで出力する
@@ -70,20 +63,43 @@ console.log('each関数の戻り値: ', returnedValueOfEach);
  */
 
 // ここでmap関数を実装する
-const map = (array, callback) => {
-  const newArray = [];
-  each(array, (value, index) => {
-    const newValue = callback(value, index);
-    newArray.push(newValue);
-  });
 
-  return newArray;
-};
 
 // ここでmap関数の挙動を確認する
-const returnedValueOfMap = map([1, 2, 3], (value, index) => {
-  console.log('map関数のコールバック関数内 index: ' + index, '値: ' + value, );
-  return value * 2;
-});
 
-console.log('map関数の戻り値: ', returnedValueOfMap);
+
+
+/**
+ * 課題3: 「Array.prototype.filter()」と同等の機能を持つ関数を作る
+ *   - 「Array.prototype.filter()」のドキュメント
+ *     - https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+ *   - 関数名は「filter」とする
+ *   - filter関数は次の2つの引数を持つ
+ *     - 第1引数: 配列(引数名はarrayとする)
+ *     - 第2引数: コールバック関数(引数名はcallbackとする)
+ *       - コールバック関数の内容は、filter関数の第1引数で受け取った配列をイテレーションする機能を持つ
+ *       - コールバックは次の2つの引数を持つ
+ *         - 第1引数: filter関数の第1引数で受け取った配列の各要素を1ループごとにセットする
+ *         - 第2引数: コールバック関数の第1引数で受け取った要素に対応するインデックス番号をセットする
+ *       - コールバック関数は戻り値を持つ
+ *         - コールバック関数内で真偽値(true or false)をreturnする
+ *           - trueを返した場合は、現在セットされている要素をfilter関数の戻り値である配列に格納する
+ *           - falseを返した場合は、現在セットされている要素をfilter関数の戻り値である配列に含めないようにする
+ *   - filter関数は戻り値を持つ
+ *     - filter関数の戻り値はコールバック関数の戻り値がtrueだった要素を格納した配列となる
+ *       - 例: 以下のfilteredArrayには[2, 4]という配列が格納される。
+ *         - const filteredArray = filter([1, 2, 3, 4, 5], (value, index) => {
+ *             return value % 2 === 0; // valueは「1, 2, 3, 4, 5」 と順番にセットされて、「value % 2 === 0」で偶数がtrueになるので「2, 4」trueとなり[2, 4]という配列が返る
+ *           });
+ *   - 実装したfilter関数が意図通り動くか確かめるためのコードを記述する
+ *     - filter関数の返り値をconsole.logで出力する
+ *     - filter関数内のイテレーション処理には課題1で作ったeach関数を利用する
+ *
+ * 上の文章で実装する内容を理解するよりも、「Array.prototype.filter()」のドキュメントの例を参考にfilterがどんな挙動をするのか理解するのか理解したほうが早いかも。
+ */
+
+// ここでfilter関数を作る
+
+
+// ここでfilter関数の挙動を確認する
+
